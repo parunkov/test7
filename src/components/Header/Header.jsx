e0c902afacd23c206a3db57f13664b412bb95910
parunkov <parunkov@gmail.com>
2020-09-10 15:57:44 +0500

    Начал делать анимацию хедера

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.scss';
 
+const NavigationItem = ({text, id, hoveredItem, activeItem, setActiveItem, setHoveredItem}) => {
+	let isHovered = false;
+	if (hoveredItem && hoveredItem !== activeItem) {
+		if (id >= activeItem && id <= hoveredItem - 1) {
+			isHovered = true;
+		} else if (id < activeItem && id >= hoveredItem) {
+			isHovered = true;
+		}
+	}
+	return(
+		<span className={isHovered ? "header__navigation-item-wrapper header__navigation-item-wrapper_active" : "header__navigation-item-wrapper"}>
+			<a href="#" className={id === activeItem ? "header__navigation-item header__navigation-item_active" : "header__navigation-item"} onClick={() => setActiveItem(id)} onMouseEnter={() => setHoveredItem(id)} onMouseLeave={() => setHoveredItem(0)}>{text}</a>
+			<span className="header__navigation-border-wrapper">
+				<span className="header__navigation-border"></span>
+			</span>
+		</span>
+	);
+}
+
 const Header = () => {
+	const [activeItem, setActiveItem] = useState(1);
+	const [hoveredItem, setHoveredItem] = useState(0);
 	return(
 		<div className="header">
 			<div className="header__logo"></div>
 			<div className="header__title">Первомайская</div>
 			<nav className="header__navigation">
-				<a href="#" className="header__navigation-item">О комплексе</a>
-				<a href="#" className="header__navigation-item">Особенности</a>
-				<a href="#" className="header__navigation-item">Пентхаусы</a>
-				<a href="#" className="header__navigation-item">Выбрать квартиру</a>
+				<NavigationItem id={1} text="О комплексе" hoveredItem={hoveredItem} setHoveredItem={setHoveredItem} activeItem={activeItem} setActiveItem={setActiveItem} />
+				<NavigationItem id={2} text="Особенности" hoveredItem={hoveredItem} setHoveredItem={setHoveredItem} activeItem={activeItem} setActiveItem={setActiveItem} />
+				<NavigationItem id={3} text="Пентхаусы" hoveredItem={hoveredItem} setHoveredItem={setHoveredItem} activeItem={activeItem} setActiveItem={setActiveItem} />
+				<NavigationItem id={4} text="Выбрать квартиру" hoveredItem={hoveredItem} setHoveredItem={setHoveredItem} activeItem={activeItem} setActiveItem={setActiveItem} />
 			</nav>
 			<a href="tel:8 888 888 88 88" className="header__phone">8 888 888 88 88</a>
 			<div className="header__image"></div>
