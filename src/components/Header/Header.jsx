bc6c542989bef97fb9d8051846640f537562cf3e
parunkov <parunkov@gmail.com>
2020-09-11 11:32:40 +0500

    Сделал таймаут

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './Header.scss';
 
-const NavigationItem = ({text, id, hoveredItem, activeItem, setActiveItem, setHoveredItem}) => {
-	let isHovered = false;
-	if (hoveredItem && hoveredItem !== activeItem) {
-		if (id >= activeItem && id <= hoveredItem - 1) {
-			isHovered = true;
-		} else if (id < activeItem && id >= hoveredItem) {
-			isHovered = true;
-		}
+const NavigationItem = ({text, id, activeItem, setActiveItem, delta, setDelta, underlinedItem}) => {
+	// console.log(underlinedItem);
+	// console.log(underlined);
+	const onHover = () => {
+		const deltaItems = id - activeItem;
+		setDelta(deltaItems);
 	}
 	return(
-		<span className={isHovered ? "header__navigation-item-wrapper header__navigation-item-wrapper_active" : "header__navigation-item-wrapper"}>
-			<a href="#" className={id === activeItem ? "header__navigation-item header__navigation-item_active" : "header__navigation-item"} onClick={() => setActiveItem(id)} onMouseEnter={() => setHoveredItem(id)} onMouseLeave={() => setHoveredItem(0)}>{text}</a>
+		<span className="header__navigation-item-wrapper">
+			<a href="#" className={id === activeItem ? "header__navigation-item header__navigation-item_active" : "header__navigation-item"} onClick={() => setActiveItem(id)} onMouseEnter={onHover} onMouseLeave={() => {}}>{text}
+
+			</a>
 			<span className="header__navigation-border-wrapper">
-				<span className="header__navigation-border"></span>
+				{underlinedItem && <span className="header__navigation-border"></span>}
 			</span>
 		</span>
 	);
@@ -22,16 +22,72 @@ const NavigationItem = ({text, id, hoveredItem, activeItem, setActiveItem, setHo
 
 const Header = () => {
 	const [activeItem, setActiveItem] = useState(1);
-	const [hoveredItem, setHoveredItem] = useState(0);
+	const [delta, setDelta] = useState(0);
+	const underlinedItems = [false, false, false, false];
+	const [underlined, setUnderlined] = useState(underlinedItems);
+	useEffect(() => {
+		const underlinedClone = [false, false, false, false];
+		if (delta) {
+			if (delta > 0) {
+				for (let i = 0; i < delta; i += 1) {
+					const setUnderlinedItems = () => {
+						underlinedClone[activeItem - 1 + i] = true;
+						setUnderlined([...underlinedClone]);
+					}
+					setTimeout(setUnderlinedItems, 500 * i);
+				}
+			} else {
+				for (let i = 0; i < Math.abs(delta); i += 1) {
+					const setUnderlinedItems = () => {
+						underlinedClone[activeItem - 2 - i] = true;
+						setUnderlined([...underlinedClone]);
+					}
+					setTimeout(setUnderlinedItems, 500 * i);
+				}	
+			}
+		}
+	},[delta]);
 	return(
 		<div className="header">
 			<div className="header__logo"></div>
 			<div className="header__title">Первомайская</div>
 			<nav className="header__navigation">
-				<NavigationItem id={1} text="О комплексе" hoveredItem={hoveredItem} setHoveredItem={setHoveredItem} activeItem={activeItem} setActiveItem={setActiveItem} />
-				<NavigationItem id={2} text="Особенности" hoveredItem={hoveredItem} setHoveredItem={setHoveredItem} activeItem={activeItem} setActiveItem={setActiveItem} />
-				<NavigationItem id={3} text="Пентхаусы" hoveredItem={hoveredItem} setHoveredItem={setHoveredItem} activeItem={activeItem} setActiveItem={setActiveItem} />
-				<NavigationItem id={4} text="Выбрать квартиру" hoveredItem={hoveredItem} setHoveredItem={setHoveredItem} activeItem={activeItem} setActiveItem={setActiveItem} />
+				<NavigationItem 
+					id={1} 
+					text="О комплексе" 
+					activeItem={activeItem} 
+					setActiveItem={setActiveItem} 
+					delta={delta}
+					setDelta={setDelta}
+					underlinedItem={underlined[0]}
+				/>
+				<NavigationItem 
+					id={2} 
+					text="Особенности" 
+					activeItem={activeItem} 
+					setActiveItem={setActiveItem}
+					delta={delta}
+					setDelta={setDelta} 
+					underlinedItem={underlined[1]}
+				/>
+				<NavigationItem 
+					id={3} 
+					text="Пентхаусы" 
+					activeItem={activeItem} 
+					setActiveItem={setActiveItem} 
+					delta={delta}
+					setDelta={setDelta}
+					underlinedItem={underlined[2]}
+				/>
+				<NavigationItem 
+					id={4} 
+					text="Выбрать квартиру" 
+					activeItem={activeItem} 
+					setActiveItem={setActiveItem} 
+					delta={delta}
+					setDelta={setDelta}
+					underlinedItem={underlined[3]}
+				/>
 			</nav>
 			<a href="tel:8 888 888 88 88" className="header__phone">8 888 888 88 88</a>
 			<div className="header__image"></div>
