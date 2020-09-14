2be342db4ba30d126f8e0b9de37b094e7aec632b
parunkov <parunkov@gmail.com>
2020-09-14 17:26:55 +0500

    Updates

---
+++ precache-manifest.f7e70cf3b040d14f08aefff491125a43.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e8f627d5d68811e52711aa71cb4823",
+    "url": "/test7/index.html"
+  },
+  {
+    "revision": "bf4de05d2cd0676d1677",
+    "url": "/test7/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "ba58d2db70bcd9ed8e13",
+    "url": "/test7/static/css/main.4f552099.chunk.css"
+  },
+  {
+    "revision": "bf4de05d2cd0676d1677",
+    "url": "/test7/static/js/2.e981e0a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test7/static/js/2.e981e0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba58d2db70bcd9ed8e13",
+    "url": "/test7/static/js/main.e51e663b.chunk.js"
+  },
+  {
+    "revision": "e29438fe398652041e49",
+    "url": "/test7/static/js/runtime-main.4e8dbabb.js"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/test7/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/test7/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/test7/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/test7/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/test7/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "878a5dec2acb0dee01d2df2192aba911",
+    "url": "/test7/static/media/architecture.878a5dec.jpg"
+  },
+  {
+    "revision": "550281cbac8b8db8f68f8e820da25e95",
+    "url": "/test7/static/media/engineering.550281cb.jpg"
+  },
+  {
+    "revision": "3ff9cfdf23fd9c4c24195fb50e2b41a3",
+    "url": "/test7/static/media/infrastructure.3ff9cfdf.jpg"
+  },
+  {
+    "revision": "ae629e52807ee3929927f873a56dd62e",
+    "url": "/test7/static/media/landscaping.ae629e52.jpg"
+  },
+  {
+    "revision": "49cb1488268ceac0245d57366e88a5c7",
+    "url": "/test7/static/media/security.49cb1488.jpg"
+  },
+  {
+    "revision": "11b62583ea69b24d094a583d1690633c",
+    "url": "/test7/static/media/transport.11b62583.jpg"
+  }
+]);
